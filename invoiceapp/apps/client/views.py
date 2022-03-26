7a2f95bd97ec94c2ca9de5239a1d40c51834fd0d
Yash Prakash <yashprakash13@gmail.com>
2022-03-26 12:17:41 -0400

    another attempt.

--- invoiceapp/apps/client/views.py
+++ invoiceapp/apps/client/views.py
@@ -1,14 +1,25 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import ClientSerializer
 from .models import Client
+from .serializers import ClientSerializer
 
 
 class ClientViewSet(viewsets.ModelViewSet):
-    serializer_class = ClientSerializer 
+    serializer_class = ClientSerializer
     queryset = Client.objects.all()
 
     def get_queryset(self):
-        return self.queryset.filter(created_by = self.request.user)
+        return self.queryset.filter(created_by=self.request.user)
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        obj = self.get_object()
+
+        if obj.created_by != self.request.user:
+            raise PermissionDenied("Wrong object owner.")
+
+        serializer.save()
